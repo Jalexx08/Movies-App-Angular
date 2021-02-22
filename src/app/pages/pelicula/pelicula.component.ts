19acad35909ab384b249bc1171321b4e61aff8f0
Jalexss <jhalx2198@gmail.com>
2021-02-22 17:42:48 -0500

    First commit

---
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { PeliculasService } from '../../services/peliculas.service';
+import { MovieResponse } from '../../interfaces/movie-response';
+import { Cast } from '../../interfaces/credits-response';
+
+@Component({
+  selector: 'app-pelicula',
+  templateUrl: './pelicula.component.html',
+  styleUrls: ['./pelicula.component.css']
+})
+export class PeliculaComponent implements OnInit {
+
+  movie: MovieResponse;
+  cast: Cast[] = [];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private peliculasService: PeliculasService,
+    private location: Location,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    const { id } = this.activatedRoute.snapshot.params;
+    this.peliculasService.getPeliculaDetalle(id).subscribe(
+      movie => {
+        if (!movie) {
+          this.router.navigateByUrl('/home');
+          return;
+        };
+        this.movie = movie;
+        // console.log(movie);
+      }
+    );
+
+    this.peliculasService.getCast(id).subscribe(
+      cast => {
+        this.cast = cast.filter( actor => actor.profile_path !== null);
+        // console.log(cast);
+      }
+    );
+
+  }
+
+  onRegresar() {
+    this.location.back();
+  }
+
+}
---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,81 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators'
+import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
+import { CreditsResponse, Cast } from '../interfaces/credits-response';
+import { MovieResponse } from '../interfaces/movie-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
+  public cargando: boolean = false;
+
+  constructor(private http: HttpClient) { }
+
+  get params() {
+    return {
+      api_key: 'd1f718df197ee42df78bcf97d331e205',
+      language: 'es-ES',
+      page: this.carteleraPage.toString()
+    }
+  }
+
+  resetCarteleraPAge() {
+    this.carteleraPage =1;
+  }
+
+  getCartelera(): Observable<Movie[]> {
+
+    if (this.cargando) {
+      return of([]);
+    }
+
+    this.cargando = true;
+
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`, {
+      params: this.params
+    }).pipe(
+      map((resp) => resp.results),
+      tap(() => {
+        this.carteleraPage += 1;
+        this.cargando = false;
+      })
+    )
+  }
+
+  buscarPeliculas(query: string): Observable<Movie[]> {
+
+    const params = { ...this.params, page: '1', query };
+
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/search/movie`, {
+      params
+    }).pipe(
+      map(resp => resp.results)
+    )
+  }
+
+  getPeliculaDetalle( id: string ) {
+
+    return this.http.get<MovieResponse>(`${this.baseUrl}/movie/${ id }`, {
+      params: this.params
+    }).pipe(
+      catchError( err => of(null))
+    );
+    
+  }
+  getCast( id: string ): Observable<Cast[]> {
+
+    return this.http.get<CreditsResponse>(`${this.baseUrl}/movie/${ id }/credits`, {
+      params: this.params
+    }).pipe(
+      map( resp => resp.cast ),
+      catchError( err => of([]))
+    );
+  }
+
+}
